7f5bf0a1758810b4a6e08da5c5dacd465b08d018
mirzagibran221 <haqimirza21@gmail.com>
2019-12-02 21:24:02 +0700

    first commit

---
+++ kal/gibs.js
@@ -0,0 +1,32 @@
+<html>
+</head>
+</head>
+<body>
+<input type="text" placeholder="0" readonly id="output">
+<input type="text" value="0" readonly id="riwayat">
+<button class="f_orange">AC</button>
+				<button class="f_orange f_big f_pad">←</button>
+				<button>+/-</button>
+				<button class="f_big" onclick="operator('/')">÷</button>
+				<button onclick = "operand(9)" >9</button>
+				<button onclick = "operand(8)" >8</button>
+				<button onclick = "operand(7)" >7</button>
+				<button onclick = "operand(6)" >6</button>
+				<button onclick = "operand(5)" >5</button>
+				<button onclick = "operand(4)" >4</button>
+				<button onclick = "operand(3)" >3</button>
+				<button onclick = "operand(2)" >2</button>
+				<button onclick = "operand(1)" >1</button>
+				<button>.</button>
+				<button onclick = "operand(0)" >0</button>
+				<button>%</button>
+				<button onclick="operator('*')">X</button>
+				<button onclick="operator('-')">-</button>
+				<button onclick="operator('+')">+</button>
+				<button onclick="hitung()">=</button>
+				<input type="text" id="sekarang">
+		<input type="text" id="operasi">
+		
+		
+</body>
+</html>
